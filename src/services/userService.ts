da753e9d111915541ea53f69220874cbe12e621f
Anil Kumar <128301859+anilkumarjnv@users.noreply.github.com>
2026-07-06 21:40:34 +0530

    chore: add EAS preview build config; android package + avatar

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -10,7 +10,7 @@ const CURRENT_USER: User = {
   id: 'u1',
   name: 'Anil Kumar',
   email: 'anil@learnhub.app',
-  avatar: 'https://i.pravatar.cc/240?img=15',
+  avatar: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   memberSince: '2024',
   enrolledCount: 6,
 };
